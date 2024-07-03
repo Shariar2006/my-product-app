b66ec162d4763deaf76f03f5a1d35e8eebf23ca5
Shariar2006 <shariarnafis86@gmail.com>
2024-07-03 09:19:01 +0600

    edit product done

--- src/components/EditProduct.tsx
+++ src/components/EditProduct.tsx
@@ -1,11 +1,77 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetProductByIdQuery, useGetCategoriesQuery, useUpdateProductMutation } from '../redux/api';
+import { Form, Input, Button, Select } from 'antd';
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom';
 
-const EditProduct = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+
+const EditProduct: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data: product, isLoading: productLoading } = useGetProductByIdQuery(id);
+  const { data: categories, isLoading: categoriesLoading } = useGetCategoriesQuery([]);
+  const [updateProduct] = useUpdateProductMutation();
+  const navigate = useNavigate();
+
+
+  const onFinish = (values: any) => {
+    console.log('Received values: ', values);
+    updateProduct({ id, ...values });
+    Swal.fire({
+        icon: "success",
+        title: "Good Job",
+        text: "Your change has been saved",
+      });
+      navigate('/')
+  };
+
+  if (productLoading || categoriesLoading) return <div>Loading...</div>;
+
+  return (
+    <Form style={{marginTop: '10px', maxWidth: '500px', marginLeft: 'auto', marginRight: 'auto', background:'#fff', padding: '25px', borderRadius: '10px'}} layout="vertical" onFinish={onFinish} initialValues={product}>
+      <Form.Item name="title" label="Name" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="price" label="Price" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="description" label="Description" rules={[{ required: true }]}>
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item name="category" label="Category" rules={[{ required: true }]}>
+        <Select>
+          {categories.map((category: {name: string, slug: string}) => (
+            <Select.Option key={category.name} value={category.slug}>
+              {category.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.List name="reviews">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map(({ key, name, fieldKey, ...restField }) => (
+              <Form.Item key={key} label="Review">
+                <Form.Item {...restField} name={[name, 'review']} fieldKey={[fieldKey!, 'review']} noStyle>
+                  <Input />
+                </Form.Item>
+                <Button style={{backgroundColor:'#28C8A4', color:'#fff', marginTop: '10px'}} onClick={() => remove(name)}>Remove</Button>
+              </Form.Item>
+            ))}
+            <Button type="dashed" onClick={() => add()} block>
+              Add Review
+            </Button>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button style={{backgroundColor:'#28C8A4', marginTop: '10px'}} type="primary" htmlType="submit">
+          Save
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default EditProduct;
--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,20 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/' }),
-  endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: ({ limit = 10, skip = 0 }) => `products?limit=${limit}&skip=${skip}`,
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/' }),
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: ({ limit = 10, skip = 0 }) => `products?limit=${limit}&skip=${skip}`,
+        }),
+        getProductById: builder.query({
+            query: (id) => `products/${id}`,
+        }),
+        getCategories: builder.query({
+            query: () => 'products/categories',
+        }),
+        updateProduct: builder.mutation({
+            query: ({ id, ...patch }) => ({
+                url: `products/${id}`,
+                method: 'PATCH',
+                body: patch,
+            }),
+        }),
     }),
-    getProductById: builder.query({
-        query: (id) => `products/${id}`,
-      }),
-    }),
-  })
+})
 
 
 export const {
-  useGetProductsQuery,
-  useGetProductByIdQuery,
+    useGetProductsQuery,
+    useGetProductByIdQuery,
+    useGetCategoriesQuery,
+    useUpdateProductMutation,
 } = api;
